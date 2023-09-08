eeaa09947ecd4cb63d717931d6ec2bd93a2a56dd
eliseolozanowp <124175479+eliseolozanowp@users.noreply.github.com>
2023-09-08 16:35:19 +0000

    Modificado

--- server.py
+++ server.py
@@ -5,19 +5,18 @@ port = 3000
 class miServer(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path=="/":
-            self.path = 'index.html'
+            self.path = "index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
-        def do_POST(self):
-            longitud = int(self.headers["Content-Lenght"])
-            datos = self.rfile.read(longitud)
-            datos = datos.decode()
-            datos = parse.unquote(datos)
-            print(datos)
-            
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(datos.encode())
-            
-print("Ejecutando server en puerto ", port)
+    def do_POST(self):
+        longitud = int(self.headers["Content-Length"])
+        datos= self.rfile.read(longitud)
+        datos = datos.decode()
+        datos = parse.unquote(datos)
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(datos.encode())
+        
+print("Ejecuntando server en puerto ", port)
 server = HTTPServer(("localhost", port), miServer)
 server.serve_forever()
