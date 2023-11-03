5971b621d9164d7f3f03661458f35bb9bf9062d1
eliseolozanowp <124175479+eliseolozanowp@users.noreply.github.com>
2023-11-03 15:19:41 +0000

    cambios

--- appacademica/appacademica/settings.py
+++ appacademica/appacademica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "appalumnos",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,9 @@ WSGI_APPLICATION = "appacademica.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dbacademica.sqlite3',
     }
 }
 
